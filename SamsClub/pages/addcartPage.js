cc22a6bdef9360374e573df28fabcd9cdca23904
PatyRome <alma.romero@wizeline.com>
2022-02-24 13:23:21 -0600

    Update addcartPage.js

--- SamsClub/pages/addcartPage.js
+++ SamsClub/pages/addcartPage.js
@@ -10,6 +10,14 @@ class AddtoCart{
         this.cart = Selector('#header-cart')
 
     }
+
+    async AddItem(){
+        await t.click(this.loginButton)
+        await t.click(this.searchClick)
+        await t.typeText(this.searchInput)
+        await t.click(this.addItem)
+        await t.cart(this.cart)
+    }
 }
 
 export default new AddtoCart();
